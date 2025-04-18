a7239b0ea4fc3ebb7403813fd63e865a8f4f71b9
erMisetto <deromamisetto@gmail.com>
2025-04-18 23:28:52 +0100

    Up

---
+++ models/core/dim_destination_port.sql
@@ -0,0 +1,17 @@
+{{
+  config(
+    materialized = 'table',
+    unique_key   = 'port_code',
+    schema       = 'core'
+  )
+}}
+
+select
+    port_code,
+    port_city,
+    port_country,
+    latitude,
+    longitude,
+    current_timestamp() as load_ts
+from {{ ref('dim_port') }}
+where port_roles_list like '%Destination%'
